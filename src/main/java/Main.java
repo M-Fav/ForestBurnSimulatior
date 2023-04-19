1c1e1be2cdc1d71e3c65cc4a0867ca133b9c9e81
MehdiFav <mehdi.favreau@gmail.com>
2023-04-19 16:45:41 +0200

    Add simulation.run at Main + librairies for command line

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@
 
 import config.ConfigReader;
 import config.ConfigData;
-
+import simulation.Simulation;
 
 
 
@@ -10,12 +10,21 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
         // création d'une instance de ConfigReader pour lire les paramètres depuis le fichier de configuration
         ConfigReader configReader = new ConfigReader();
         ConfigData config = configReader.readConfig("C:\\WORK-SRC\\ForestBurnSimulator\\src\\main\\resources\\config.json");
 
+        // création d'une instance de Simulation en utilisant l'objet ConfigData
+        Simulation simulation = new Simulation(config);
 
+        // exécution de la simulation
+        simulation.run();
+
+        // affichage des résultats de la simulation
+        int burnedCellsCount = simulation.getBurnedCellsCount();
+        int stepsCount = simulation.getStepsCount();
+        System.out.println("Simulation finished in " + stepsCount + " steps");
+        System.out.println("Number of burned cells: " + burnedCellsCount);
     }
 }
 
